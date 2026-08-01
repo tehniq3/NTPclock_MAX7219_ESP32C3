abe2ca0229eebd3952b50de51361302167ecb9d6
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2026-08-01 13:15:07 +0300

    Change default SSID and password in WiFiConfig.h
    Updated default WiFi credentials in WiFiConfig.h.

--- esp32c3_max7219_clock/WiFiConfig.h
+++ esp32c3_max7219_clock/WiFiConfig.h
@@ -3,7 +3,7 @@
 #define WIFI_CONFIG_H
 
 // Default WiFi credentials — change these or set via web config
-#define DEFAULT_SSID "bbk2"
-#define DEFAULT_PASSWORD "internet2"
+#define DEFAULT_SSID "WIFI_name"
+#define DEFAULT_PASSWORD "XXXXXX"
 
 #endif // WIFI_CONFIG_H
